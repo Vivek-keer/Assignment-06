6a29c8ec5a2618d953446d4206395c1deac704c9
Vivek-keer <108209332+Vivek-keer@users.noreply.github.com>
2022-07-23 20:42:35 +0530

    Update main.c

--- Assignment-06/prime or not/main.c
+++ Assignment-06/prime or not/main.c
@@ -5,16 +5,15 @@ int main()
    printf("Enter the number ");
 
    scanf("%d",&x);
-   for(m=1,n=2;m<=3;m++)
+   for(m=1,n=2;m<=4;m++)
    {
        if(x==1) {printf("Given number is not prime"); break;}
        if(x/n==1) {printf("Given number is prime"); break;}
-       if(x%n) n=2*m+1;
-       else {printf("Given number is not prime"); break;}
+       if(x%n) {n=2*m+1; }
+       else {printf("Given number is not prime"); m=1; break;}
    }
-   if(m==4) printf("Given number is prime");
+    if(m==4) printf("Given number is prime");
    return 0;
 }
-
 
 
